f5919e777327b314e566fbd0c651c2edf8cb1502
Ivan Carneiro <ivanexequielc@gmail.com>
2024-05-13 22:52:32 -0300

    remove package never use

--- Clase05/src/main/java/ar/org/centro35/curso/java/clase04/entities/Profesor.java
+++ Clase05/src/main/java/ar/org/centro35/curso/java/clase04/entities/Profesor.java
@@ -2,9 +2,7 @@ package ar.org.centro35.curso.java.clase04.entities;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+
 
 //@ToString
 //@Getter
